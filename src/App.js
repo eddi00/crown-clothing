ea5e4df0ae4d6c75d1a2318984668721697c697f
ed <eduard.beliaev@gmail.com>
2022-01-01 15:30:23 +0500

    Added shop page + collection preview component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<HomePage />} />
-      <Route path="/hats" element={<HatsPage />} />
+      <Route path="/shop" element={<ShopPage />} />
     </Routes>
   );
 }
